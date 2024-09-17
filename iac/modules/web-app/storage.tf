be50169fa0445ee1a92f56da3728bef76ad49df3
khcatino <kcatino@itba.edu.ar>
2024-09-17 20:27:01 -0300

    s3 static page

--- iac/modules/web-app/storage.tf
+++ iac/modules/web-app/storage.tf
@@ -20,34 +20,40 @@ resource "aws_s3_bucket_versioning" "logs" {
   }
 }
 
-# resource "aws_s3_bucket_policy" "logs" {
-#   bucket   = aws_s3_bucket.logs.id
+data "aws_iam_policy_document" "root_policy" {
+  statement {
+    sid    = "AllowPublicRead"
+    effect = "Allow"
+resources = [
+      "${aws_s3_bucket.root.arn}",
+      "${aws_s3_bucket.root.arn}/*",
+    ]
+actions = ["S3:GetObject"]
+principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
 
-#      policy = <<POLICY
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Sid": "AllowCloudFrontToPutLogs",
-#       "Effect": "Allow",
-#       "Principal": {
-#         "Service": "cloudfront.amazonaws.com"
-#       },
-#       "Action": "s3:PutObject",
-#       "Resource": "arn:aws:s3:::logs.${var.domain_name}/*",
-#       "Condition": {
-#         "StringEquals": {
-#           "aws:SourceArn": [
-#             "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.website["root"].id}",
-#             "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.website["www"].id}"
-#           ]
-#         }
-#       }
-#     }
-#   ]
-# }
-# POLICY
-# }
+  depends_on = [aws_s3_bucket_public_access_block.website_allow_access]
+}
+
+data "aws_iam_policy_document" "www_policy" {
+  statement {
+    effect = "Allow"
+    resources = [
+      "${aws_s3_bucket.www.arn}",
+      "${aws_s3_bucket.www.arn}/*",
+    ]
+actions = ["S3:GetObject"]
+principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+
+  depends_on = [aws_s3_bucket_public_access_block.website_allow_access]
+}
 
 locals {
   bucket_ids = {
@@ -59,10 +65,10 @@ locals {
     log = aws_s3_bucket.logs.id
   }
 
-  # bucket_policies = {
-  #   www  = templatefile("${path.module}/files/s3-policy.json", { bucket = "www.${var.domain_name}", account_id = data.aws_caller_identity.current.account_id, cloudfront_id = aws_cloudfront_distribution.website["www"].id })
-  #   root = templatefile("${path.module}/files/s3-policy.json", { bucket = "${var.domain_name}", account_id = data.aws_caller_identity.current.account_id, cloudfront_id = aws_cloudfront_distribution.website["root"].id })
-  # }
+  bucket_policies = {
+    www  = data.aws_iam_policy_document.www_policy.json
+    root = data.aws_iam_policy_document.root_policy.json
+  }
 }
 
 resource "aws_s3_bucket_ownership_controls" "website" {
@@ -71,49 +77,34 @@ resource "aws_s3_bucket_ownership_controls" "website" {
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
+  depends_on = [aws_s3_bucket_public_access_block.website_allow_access]
 }
 
 resource "aws_s3_bucket_public_access_block" "website_allow_access" {
   for_each = local.bucket_ids
   bucket   = each.value
 
-  block_public_acls       = true
+  block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
 
   depends_on = [aws_s3_bucket.root, aws_s3_bucket.www, aws_s3_bucket.logs]
-  lifecycle {
-    ignore_changes = [block_public_acls, block_public_policy, ignore_public_acls, restrict_public_buckets]
-  }
 }
 
-resource "aws_s3_bucket_public_access_block" "website_block_access" {
-  for_each = merge(local.bucket_ids, local.log_bucket_id)
+resource "aws_s3_bucket_policy" "website" {
+  for_each = local.bucket_ids
   bucket   = each.value
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-
-  # depends_on = [aws_s3_bucket_policy.website]
+  policy     = local.bucket_policies[each.key]
+  depends_on = [aws_s3_bucket_acl.website]
 }
 
-# resource "aws_s3_bucket_policy" "website" {
-#   for_each = local.bucket_ids
-#   bucket   = each.value
-
-#   policy     = local.bucket_policies[each.key]
-#   depends_on = [aws_s3_bucket_acl.website]
-# }
-
-# resource "aws_s3_bucket_acl" "website" {
-#   for_each   = local.bucket_ids
-#   bucket     = each.value
-#   acl        = "public-read"
-#   depends_on = [aws_s3_bucket_public_access_block.website_allow_access]
-# }
+resource "aws_s3_bucket_acl" "website" {
+  for_each   = local.bucket_ids
+  bucket     = each.value
+  acl        = "public-read"
+  depends_on = [aws_s3_bucket_ownership_controls.website, aws_s3_bucket_public_access_block.website_allow_access]
+}
 
 resource "aws_s3_bucket_cors_configuration" "website" {
   bucket = aws_s3_bucket.www.id
@@ -141,7 +132,7 @@ resource "aws_s3_bucket_website_configuration" "www" {
 resource "aws_s3_bucket_website_configuration" "root" {
   bucket = aws_s3_bucket.root.id
   redirect_all_requests_to {
-    host_name = "https://www.wikipedia.com" // En realidad, iria a nuestro domain_name, pero no tenemos certificado
+    host_name = aws_s3_bucket.www.bucket_domain_name
   }
 }
 
