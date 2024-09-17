3ae23231eeea9ebd05ded2178cce452312ad33dc
AleCa <acaeiro@itba.edu.ar>
2024-09-16 23:52:44 -0300

    First version css

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import Link from 'next/link'
+import SearchBar from './SearchBar'
 
 export default function Navbar() {
-    return <header className='bg-blue-500 w-full h-12 flex items-center'>
+    return <header className='bg-secondary font-serif w-full h-12 flex items-center'>
       <Link href={'/'} className='px-2 font-bold'>
       Inventory System
       </Link>
+      
+      <div className="flex-1 flex justify-center">
+        <SearchBar />
+      </div>
+      
     </header>
   }
