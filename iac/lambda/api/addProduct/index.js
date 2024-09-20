e24070df32d05694dd86ec7b4a37376279f0f146
IanSzejer <ianszejer@gmail.com>
2024-09-20 17:29:48 -0300

    Craeting addProduct lambda

---
+++ iac/lambda/api/addProduct/index.js
@@ -0,0 +1,48 @@
+const { Client } = require('pg');
+
+exports.handler = async (event) => {
+    const client = new Client({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    });
+
+    if (!event.body){
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: "Request should have a body with fields productName, productPrice y productStockAmount are mandatory" }),
+        };
+    }
+    const body = JSON.parse(event.body);
+
+    const { productName, productPrice, productStockAmount, productDescription} = body;
+
+    if (!productName || !productPrice || !productStockAmount || !productDescription ){
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: "Fields productName, productPrice y productStockAmount are mandatory" }),
+        };
+    }
+    
+    await client.connect();
+
+    try{
+        const query = `INSERT INTO product (name,price,stock,description) VALUES($1,$2,$3,$4)`
+        const values = [productName,productPrice,productStockAmount,productDescription]
+        await client.query(query,values);
+        await client.end();
+    }catch(error){
+        await client.end();
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Error adding product.", error: error.message }),
+        };
+    }
+    
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Succesfull product adding" }),
+    };
+};
