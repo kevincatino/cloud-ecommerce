94e4ae3edc1814b5b222cd5eb8f74641ee13ef73
AleCa <acaeiro@itba.edu.ar>
2024-09-22 14:57:33 -0300

    Functional get, add & delete

--- frontend/src/components/ItemAction.jsx
+++ frontend/src/components/ItemAction.jsx
@@ -6,7 +6,8 @@ const ActionButton = ({
     hasInput, 
     onButtonClick, 
     placeholder, 
-    isGetButton,
+    hasBorderRight,
+    hasBorderLeft,
     inputValue,
     setInputValue,
     addResponse,
@@ -20,45 +21,60 @@ const ActionButton = ({
         }
       };
 
-    const handleInputChange = (event) => {
-        setInputValue(event.target.value);
+    const handleInputChange = (event, k) => {
+        console.log(k)
+        setInputValue({
+            ...inputValue, 
+            [k]: event.target.value
+        }
+        );
     };
 
     return (
-        <div className="flex flex-col items-center justify-start ${isGetButton ? 'border-l-2 border-r-2 border-white' : ''}" style={{ width: '33.33vw', height: '100%' }}>
+        <div className={`flex flex-col items-center justify-start ${hasBorderRight ? 'border-r-2 border-white' : ''} ${hasBorderLeft ? 'border-l-2 border-white' : ''}`} style={{ width: '33.33vw', height: '100%' }}>
         <button
-            className={`bg-${buttonColor}-500 hover:bg-${buttonColor}-700 text-white font-bold py-2 px-4 rounded mt-4`}
+            className={`text-white font-bold py-2 px-4 rounded mt-4`}
             onClick={onButtonClick}
+            style={{backgroundColor: buttonColor}}
         >
             {buttonText}
         </button>
         {hasInput && (
-            <input 
-            className="mt-4 px-3 py-2 border border-gray-300 rounded-lg" 
-            type="text" 
-            placeholder={placeholder}
-            value={inputValue}
-            onKeyDown={handleKeyDown}
-            onChange={handleInputChange}
-            />
+            Object.keys(inputValue).map(k =>
+                <input 
+                    key={k}
+                    className="mt-4 px-3 py-2 border border-gray-300 rounded-lg" 
+                    type="text" 
+                    placeholder={placeholder + " " + k.replace(/([A-Z])/g, ' $1').toLowerCase()}
+                    value={inputValue[k]}
+                    onKeyDown={handleKeyDown}
+                    onChange={(e) => handleInputChange(e, k)}
+                />
+            )
+            
         )}
         {addResponse && (
             <div className="mt-4 p-4 bg-gray-800 text-white border border-gray-600 rounded-lg w-11/12">
                 <p className="text-lg font-semibold mb-2">Response:</p>
-                <p>ID: {addResponse.id}</p>
-                <p>Title: {addResponse.title}</p>
+                <p>Status: {addResponse.message}</p>
             </div>
         )}
 
         {getResponse && getResponse.length > 0 && (
-            <div className="mt-4 p-4 bg-gray-800 text-white border border-gray-600 rounded-lg w-11/12">
-                <p className="text-lg font-semibold mb-2">Fetched Items:</p>
-                <ul>
-                    {getResponse.map(item => (
-                        <li key={item.id} className="text-sm">ID: {item.id}, Title: {item.title}</li>
-                    ))}
-                </ul>
-            </div>
+            getResponse.map(item => (
+
+                <div className="mt-4 p-4 bg-gray-800 text-white border border-gray-600 rounded-lg w-11/12">
+                    <p className="text-lg font-semibold mb-2">Item {item.id}:</p>
+                
+                            <ul key={item.id}>
+                                <li className="text-sm"> Title: {item.name}</li>
+                                <li className="text-sm"> Price: {item.price}</li>
+                                <li className="text-sm"> Stock: {item.stock}</li>
+                                <li className="text-sm"> Description: {item.description}</li>
+                            </ul>
+                    
+                </div>
+            ))
         )}
 
         
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import SearchBar from './SearchBar'
 export default function Navbar() {
     return <header className='bg-secondary font-serif w-full h-12 flex items-center'>
       <Link href={'/'} className='px-2 font-bold'>
-      Inventory System
+      E-commerce System
       </Link>
       
     </header>
